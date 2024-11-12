13e396c2c05aacf748d8a0b4b9a5d1bfdb30686f
shahzodbeeck <shahzodomonboyev1.com>
2024-11-12 22:13:24 +0500

    asd

---
+++ Users/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Users(AbstractUser):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    SEX_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+    )
+    sex = models.CharField(max_length=255, choices=SEX_CHOICES)
+    born_date = models.DateField()
+    born_address = models.CharField(max_length=255)
+    indefikatsiya_pin = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    passport_pdf1 = models.FileField(upload_to='passport/', null=True, blank=True)
+    passport_pdf2 = models.FileField(upload_to='passport/', null=True, blank=True)
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    )
+    role = models.CharField(max_length=255, choices=ROLE_CHOICES)
+    email = models.EmailField(unique=True)
+    phone_extra = models.CharField(max_length=255)
+    passport_seria = models.CharField(max_length=255)
