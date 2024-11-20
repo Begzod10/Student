e77cb8faa3d29ad3f133059e05b58579e9bef04f
Begzod 10 <rimefara22@gmail.com>
2024-11-20 23:22:15 +0500

    commit

--- organizations/organization_landing_page/serializers/crud/crud.py
+++ organizations/organization_landing_page/serializers/crud/crud.py
@@ -2,26 +2,40 @@ from rest_framework import serializers
 
 from organizations.models.organization_landing_page import OrganizationLandingPage
 from organizations.organization.serializers.get.retrieve_view import OrganizationSerializerForLanding
-from students.models import AcademicYear
+from students.models.academic_year import AcademicYear
+from organizations.models.models import OrganizationDegrees
 
 
 class OrganizationLandingPageSerializer(serializers.ModelSerializer):
-    organizations = OrganizationSerializerForLanding()
+    organization = OrganizationSerializerForLanding()
 
+    # degree =
     class Meta:
         model = OrganizationLandingPage
         fields = [
             'id',
-            'organizations',
+            'organization',
             'year_id',
             'desc',
             'name_optional',
             'expire_date',
-            'degree_id'
+            'degree'
         ]
         depth = 1
 
     def create(self, validated_data):
         year = validated_data.pop('year')
+        degree = validated_data.pop('degree')
         get_year = AcademicYear.objects.get(year=year)
-        return OrganizationLandingPage.objects.create(**validated_data, year_id=get_year)
+        get_degree = OrganizationDegrees.objects.get(degree=degree)
+        return OrganizationLandingPage.objects.create(**validated_data, year_id=get_year, degree_id=get_degree)
+
+    def update(self, instance, validated_data):
+        year = validated_data.pop('year')
+        degree = validated_data.pop('degree')
+        get_year = AcademicYear.objects.get(year=year)
+        get_degree = OrganizationDegrees.objects.get(degree=degree)
+        instance.year_id = get_year
+        instance.degree_id = get_degree
+        instance.save()
+        return instance  # Return the updated instance
