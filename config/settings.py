5e443547a9af2662fb60f07bb92be6041cdc3c24
thesobirov <shahzodsobirjonov9@gmail.com>
2025-01-20 15:03:41 +0500

    afa

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
     'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework_simplejwt.authentication.JWTAuthentication',
     # ],
--- organizations/organization/api/crud/destroy.py
+++ organizations/organization/api/crud/destroy.py
@@ -4,6 +4,6 @@ from organizations.models import Organization
 from organizations.organization_type.serializers.get.retrieve_view import OrganizationTypeSerializer
 
 
-class OrganizationTypeDestroyApiView(generics.DestroyAPIView):
+class OrganizationTypeDestroyApiView(generics.UpdateAPIView):
     queryset = Organization.objects.all()
     serializer_class = OrganizationTypeSerializer
