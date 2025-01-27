d02f13dc61161940a154a23ac3e6948f4bd4b2e2
shahzodbeeck <shahzodomonboyev1.com>
2025-01-27 20:52:41 +0500

    asdad

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-x8p_w1cymrqrta$7y!il0$k(-y0hi=log22cu7m#y5g1drwhid'
 from datetime import timedelta
+import os
 
 DEBUG = True
 
@@ -116,6 +117,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
