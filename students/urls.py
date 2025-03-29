04f2e4f94922acb429f1b172d4101939e33c881b
shahzodbeeck <shahzodomonboyev1.com>
2025-03-29 14:40:29 +0500

    sada

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
 from students.student_requests.views import StudentRequestListView, StudentRequestRetrieveView, \
     student_request_dashboard
-from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, include
+
+from students.student_requests.views import StudentRequestListView, StudentRequestRetrieveView, \
+    student_request_dashboard
 
 urlpatterns = [
     path('student_request_list/', StudentRequestListView.as_view(), name='student-request-list'),
@@ -10,7 +12,7 @@ urlpatterns = [
     path('student_requests/', include('students.student_requests.urls')),
     path('region/crud/', include('students.region.api.crud.urls')),
     path('student_request/<int:pk>/', StudentRequestRetrieveView.as_view(), name='student-request'),
-    path('acedem.ic_year/', include('students.academic_year.api.get.urls')),
+    path('acedemic_year/', include('students.academic_year.api.get.urls')),
     path('shift/', include('students.shift.api.get.urls')),
     path('notification/', include('students.notification.urls')),
 ]
