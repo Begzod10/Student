dc3d267d1dc89ae613e1ccfd08ee13b564c7ef3a
shahzodbeeck <shahzodomonboyev1.com>
2025-04-04 20:32:05 +0500

    fds

--- organizations/organization_fields/api/get/urls.py
+++ organizations/organization_fields/api/get/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .list_from_organization_type import OrganizationFieldsListView
+from .list_from_organization_type import OrganizationFieldsListView,OrganizationFields2ListView
 
 urlpatterns = [
 
     path('organization-fields/<int:pk>/', OrganizationFieldsListView.as_view(), name='organization-fields-list'),
+    path('organization-fields2/<int:pk>/', OrganizationFields2ListView.as_view(), name='organization-fields-list'),
 ]
