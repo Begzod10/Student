fb4c4d3d84ea9f765e82302911f83b8dab671c83
Begzod10 <rimefara22@gmail.com>
2025-04-05 21:21:33 +0500

    asfafca

--- test/block/serializers/crud/crud.py
+++ test/block/serializers/crud/crud.py
@@ -7,17 +7,30 @@ from test.models.test_block import TestBlock
 class TestQuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestQuestion
-        fields = ['id', 'block', 'test', 'isTrue', 'answer', 'to_json']
+        fields = ['id', 'block', 'test', 'isTrue', 'answer', 'to_json', 'image']
+        extra_kwargs = {'block': {'required': False}, 'test': {'required': False}}
 
 
 class TestBlockSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False, allow_null=True)
+    questions = TestQuestionSerializer(many=True, required=False)
 
     class Meta:
         model = TestBlock
-        fields = ['id', 'test', 'text', 'to_json', 'image']
+        fields = ['id', 'test', 'text', 'to_json', 'image', 'questions']
 
     def create(self, validated_data):
         image = validated_data.pop('image', None)
         block = TestBlock.objects.create(image=image, **validated_data)
         return block
+
+    def update(self, instance, validated_data):
+        questions_data = validated_data.pop('questions', [])
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        for question_data in questions_data:
+            TestQuestion.objects.create(block=instance, test=instance.test, **question_data)
+
+        return instance
