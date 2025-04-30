a659859acf287e0cec4fb6bc9ed2dcc98400e873
shahzodbeeck <shahzodomonboyev1.com>
2025-04-30 21:51:42 +0500

    dfd

--- students/region/api/get/list.py
+++ students/region/api/get/list.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 
 from students.models.region import Region, District
-from students.region.serializers.get.retrieve_view import RegionSerializer, DistrictSerializer
 from students.region.serializers.crud.crud import create_regions
+from students.region.serializers.get.retrieve_view import RegionSerializer, DistrictSerializer
 
 
 class RegionListView(generics.ListAPIView):
@@ -17,7 +17,8 @@ class DistrictListView(generics.ListAPIView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        region_id = self.request.query_params.get('region_id')
-        if region_id:
-            return queryset.filter(region_id=region_id)
+        region_ids = self.request.query_params.get('region_id')
+        if region_ids:
+            region_id_list = [int(id.strip()) for id in region_ids.split(',') if id.strip().isdigit()]
+            return queryset.filter(region_id__in=region_id_list)
         return queryset.none()
