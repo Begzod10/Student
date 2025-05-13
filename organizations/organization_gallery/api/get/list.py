0b32f2628d506faf28271ec5e1216bfa0ad44f1a
Begzod10 <rimefara22@gmail.com>
2025-05-13 20:21:24 +0500

    commit

--- organizations/organization_gallery/api/get/list.py
+++ organizations/organization_gallery/api/get/list.py
@@ -1,15 +1,14 @@
 from rest_framework import generics
 
 from organizations.models.models import OrganizationGallery
-from organizations.organization_gallery.serializers.get.retrieve_view import OrganizationGallerySerializer
+from organizations.organization.serializers.get.retrieve_view import OrganizationGallerySerializer
 
 
 class OrganizationGalleryList(generics.ListAPIView):
-    queryset = OrganizationGallery.objects.all()
     serializer_class = OrganizationGallerySerializer
 
     def get_queryset(self):
         organization_id = self.request.query_params.get('organization_id', None)
         if organization_id is not None:
             return OrganizationGallery.objects.filter(organization_id=organization_id)
-        return None
+        return OrganizationGallery.objects.none()  # Or handle differently if you want to return all
